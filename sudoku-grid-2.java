858ff5ee13aa8ed7d6d13959e380f98dfa2b42af
Jose Contreras <jocontre@joselcontreras.com>
2015-07-13 11:14:37 -0500

    Update sudoku-grid-2.java

--- sudoku-grid-2.java
+++ sudoku-grid-2.java
@@ -1,12 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Brian
- */
 public class SudokuGrid2 {
     public static void main(String[] args)
     {
