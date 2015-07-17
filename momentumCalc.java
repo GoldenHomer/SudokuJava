7ed9b262538d24e1b4e22464699736f0a55dcb15
Jose Contreras <jocontre@joselcontreras.com>
2015-07-17 10:00:10 -0500

    Create momentumCalc.java

---
+++ momentumCalc.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class MomentumCalculator {
+    //momentum = mass * velocity
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double mass;
+        double velocity;
+        double momentum;
+        System.out.println("Welcome to the momentum calculator!");
+        System.out.println("Please enter the mass (in kg):");
+        mass = input.nextDouble();
+        System.out.println("Please enter the velocity (in meters per second):");
+        velocity = input.nextDouble();
+        momentum = mass * velocity;
+        System.out.printf("The momentum is %.5f kg*m/s!\n", momentum);
+        input.close();
+    }
+}
