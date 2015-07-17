b0bd18f8d3caa05eeeef49523289aca4ef1d1111
Jose Contreras <jocontre@joselcontreras.com>
2015-07-17 10:14:40 -0500

    Update sudoku-grid-2.java

--- sudoku-grid-2.java
+++ sudoku-grid-2.java
@@ -82,5 +82,87 @@ public class SudokuGrid2 {
                             , String.format(gridData, nine, one, two)
                             , String.format(gridData, three, four, blank)
                             , String.format(gridData, six, seven, eight));
+        
+        int twoTwo;
+        int oneFive;
+        int threeEight;
+        int fourFour;
+        int fiveOne;
+        int sixSeven;
+        int sevenThree;
+        int eightNine;
+        int nineSix;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What is the value for grid one slot five?");
+        oneFive = input.nextInt();
+        
+        System.out.println("What is the value for grid two slot two?");
+        twoTwo = input.nextInt();
+        
+        System.out.println("What is the value for grid three slot eight?");
+        threeEight = input.nextInt();
+        
+        System.out.println("What is the value for grid four slot four?");
+        fourFour = input.nextInt();
+        
+        System.out.println("What is the value for grid five slot one?");
+        fiveOne = input.nextInt();
+        
+        System.out.println("What is the value for grid six slot seven?");
+        sixSeven = input.nextInt();
+        
+        System.out.println("What is the value for grid seven slot three?");
+        sevenThree = input.nextInt();
+        
+        System.out.println("What is the value for grid eight slot nine?");
+        eightNine = input.nextInt();
+        
+        System.out.println("What is the value for grid nine slot six?");
+        nineSix = input.nextInt();
+        
+        //now reprint the grid with user output:
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, one, two, three)
+                            , String.format(gridData, four, oneFive, six)
+                            , String.format(gridData, seven, eight, nine));
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, four, twoTwo, six)
+                            , String.format(gridData, seven, eight, nine)
+                            , String.format(gridData, one, two, three));
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, seven, eight, nine)
+                            , String.format(gridData, one, two, three)
+                            , String.format(gridData, four, threeEight, six));
+        System.out.println(horizontalLine);
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, two, three, four)
+                            , String.format(gridData, fourFour, six, seven)
+                            , String.format(gridData, eight, nine, one));
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, fiveOne, six, seven)
+                            , String.format(gridData, eight, nine, one)
+                            , String.format(gridData, two, three, four));
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, eight, nine, one)
+                            , String.format(gridData, two, three, four)
+                            , String.format(gridData, sixSeven, six, seven));
+        System.out.println(horizontalLine);
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, three, four, sevenThree)
+                            , String.format(gridData, six, seven, eight)
+                            , String.format(gridData, nine, one, two));
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, six, seven, eight)
+                            , String.format(gridData, nine, one, two)
+                            , String.format(gridData, three, four, eightNine));
+        System.out.printf("%s | %s | %s\n"
+                            , String.format(gridData, nine, one, two)
+                            , String.format(gridData, three, four, nineSix)
+                            , String.format(gridData, six, seven, eight));
+        
+        System.out.println("Puzzle complete, did you get it right?");
+        input.close();
     }
 }
